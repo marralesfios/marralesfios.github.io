895c71de7eb2bd0e905a8832ff18119e4c3bc925
maf <Programmer00001H@gmail.com>
2026-01-27 10:36:13 +0800

    changes

---
+++ article.ts
@@ -0,0 +1,24 @@
+let lit: boolean = localStorage.getItem("lit") === "1";
+function set_page(){
+    if(lit){
+        document.documentElement.classList.add("invert");
+    }else{
+        document.documentElement.classList.remove("invert");
+    }
+}
+set_page();
+addEventListener("DOMContentLoaded",() => {
+    document.getElementById("light")!.addEventListener("click",() => {
+        if(lit = !lit){
+            localStorage.setItem("lit","1");
+        }else{
+            localStorage.removeItem("lit");
+        }
+        set_page();
+    });
+    for(const el of document.getElementsByTagName("img")){
+        el.addEventListener("click",() => {
+            window.open(el.src);
+        });
+    }
+});
